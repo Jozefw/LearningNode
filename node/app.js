50ca62f9569ba55e3759ccdf3e674e7f65cb0211
Jozef <jozefw@sbcglobal.net>
2016-04-13 19:19:16 -0700

    add mongoose, express, mongo

--- node/app.js
+++ node/app.js
@@ -1,6 +1,5 @@
-// use is creating and switching to a new db
-// db.nameof new collection.insert( object );
-// show collection displays the models
-// db.name.find() shows the contents, findOne() shows the first one
-// db.remove(select criteria is id#)
-// db.name.update(id, object)
+var newApp = angular.module('myNewApp', []);
+
+newApp.controller('localController', ['$http','$scope', function ($http, $scope) {
+
+}]);
